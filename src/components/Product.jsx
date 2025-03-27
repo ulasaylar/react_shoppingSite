2fe434b8ba7479e35745184b0ca7cd1a3dec723f
Ulaş Aylar <ulasaylar@gmail.com>
2025-03-27 19:51:46 +0300

    css and react-router improvements

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "../css/product.css";
+import { useNavigate } from "react-router-dom";
 
 function Product({ product }) {
   const { id, price, image, title, description } = product;
+
+  const navigate = useNavigate();
   return (
     <div className="product-card">
       <img className="img" src={image} alt="" />
+
       <div className="sub-card">
-        <p className="title-p">{title}</p>
+        <div className="title-p-cont">
+          <p className="title-p">{title}</p>
+        </div>
         <p>{price} ₺</p>
+      </div>
+
+      <div className="detail-button">
+        <button onClick={() => navigate("/product-details/" + id)}>
+          Detayına Git
+        </button>
       </div>
     </div>
   );
